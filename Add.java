28563a9666ac07d9e6c2fcd5c3f2f0bb65d48968
Aravindkokkeragadda <121012777+Aravindkokkeragadda@users.noreply.github.com>
2022-12-19 14:13:01 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Add
+{
+    static public void main(String...args)
+
+{
+Scanner s=new Scanner(System.in);
+    System.out.println("enter an integer");
+    int a=s.nextInt();
+    int b=s.nextInt();
+    System.out.println("addition of" +a+" and" +b+" is: "+(a+b));
+}
+}
+
+    
